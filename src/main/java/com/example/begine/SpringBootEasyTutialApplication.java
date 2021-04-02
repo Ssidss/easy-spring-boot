6116c880e09c41262d2859c7e75c0fd97a997b82
Charles Lu <u09900148@gmail.com>
2021-04-02 12:44:54 +0800

    init

---
+++ src/main/java/com/example/begine/SpringBootEasyTutialApplication.java
@@ -0,0 +1,13 @@
+package com.example.begine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEasyTutialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEasyTutialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/begine/SpringBootEasyTutialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.begine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEasyTutialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
